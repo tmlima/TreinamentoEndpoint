78590aea0668860fddced66093628068198dcb43
Thiago Machado Lima <thiagol@dbserver.com.br>
2018-01-30 20:19:23 -0200

    Add project files.

---
+++ TreinamentoServiceApp/HelloWorldService.svc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreinamentoServiceApp
+{
+    public class HelloWorldService : IHelloWorldService
+    {
+        public string Hello( string name )
+        {
+            return "Olá " + name + "!";
+        }
+    }
+}
